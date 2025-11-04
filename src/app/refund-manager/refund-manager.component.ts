b0f350298028b9dbda07b62cd6ed6f56635fbed9
deveBrice <brice.william971@gmail.com>
2025-11-04 08:32:58 +0100

    add feature new refund

--- src/app/refund-manager/refund-manager.component.ts
+++ src/app/refund-manager/refund-manager.component.ts
@@ -1,40 +1,62 @@
-import { Component, computed, inject, model } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, computed, effect, inject, model, signal, ViewChild } from '@angular/core';
+import { FormsModule, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NewRefundComponent } from './new-refund/new-refund.component';
+
 import { RefundManagerService } from '../../shared/services/refund-manager.service';
+import { MatTable, MatTableModule } from '@angular/material/table';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms';
+import { RefundData, RefundManager, refundManagerData } from '../../shared/interfaces/refund-manager.interface';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-refund-manager',
-  imports: [MatTableModule, MatCheckboxModule, FormsModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    NewRefundComponent,
+    MatTableModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './refund-manager.component.html',
   styleUrl: './refund-manager.component.scss',
 })
 
 export class RefundManagerComponent {
+  @ViewChild(MatTable) table!: MatTable<any>;
+
   public displayedColumns: string[] = ['num', 'wording', 'amount', 'date'];
-  public checked = model();
 
-  public rms = inject(RefundManagerService)
+  public rms = inject(RefundManagerService);
 
-  public refundManagerList = computed(() => {
+  public refundList = signal<RefundManager>(this.rms.getRefundList());
 
-    return this.rms.refundManagerList().refundManagerData;
-  })
+  public refundFieldUpdate = new FormControl(0, [Validators.required])
 
+  public refundStateUpdate = signal<boolean>(false);
 
+  public newRefund($event: refundManagerData): void {
+    this.rms.addNewRefund($event);
+    this.table.renderRows();
+  }
 
-  public refundCalculate = computed(() => {
-    const initialValue = 0;
-    let refundManagerList = this.rms.refundManagerList()
-    refundManagerList.totalAmount = refundManagerList.refundManagerData.reduce(
-      (accumulator: number, currentValue: any) => {
+  public refundEdit(): void {
+    this.refundFieldUpdate.patchValue(this.refundList().totalAmount);
+    this.refundStateUpdate.set(!this.refundStateUpdate())
+  }
 
-        return accumulator + +currentValue.refundData.amount;
-      },
-      initialValue
-    );
+  public refundUpdate(): void {
+    const totalRefund = this.refundFieldUpdate.value as number;
+    this.rms.refundUpdate(totalRefund)
+    this.refundStateUpdate.set(!this.refundStateUpdate())
+  }
 
-    return refundManagerList.totalAmount;
-  })
 }
--- src/shared/services/refund-manager.service.ts
+++ src/shared/services/refund-manager.service.ts
@@ -1,19 +1,35 @@
 import { Injectable, signal } from "@angular/core";
 import { RefundManager } from '../../shared/interfaces/refund-manager.interface'
+import { refundManagerData } from "../../shared/interfaces/refund-manager.interface";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class RefundManagerService {
+    public date = new Date();
+
+    public monthsList: string[] = [
+        'Janvier',
+        'Février',
+        'Mars',
+        'Avril',
+        'Mai',
+        'Juin',
+        'Juillet',
+        'Août',
+        'Septembre',
+        'Octobre',
+        'Novembre',
+        'Décembre'
+    ]
 
     public refundManagerList = signal<RefundManager>({
-
+        month: 'Octobre',
+        year: 2025, //new Date().getFullYear()
         totalAmount: 0,
         refundManagerData: [
             {
-                month: 'Octobre',
-                year: 2025,
                 refundData: {
                     wording: 'Frais de paiement',
                     amount: 3.40,
@@ -21,8 +37,6 @@ export class RefundManagerService {
                 }
             },
             {
-                month: 'Octobre',
-                year: 2025,
                 refundData: {
                     wording: 'Frais de paiement',
                     amount: 9.47,
@@ -30,8 +44,6 @@ export class RefundManagerService {
                 }
             },
             {
-                month: 'Octobre',
-                year: 2025,
                 refundData: {
                     wording: 'Frais de paiement',
                     amount: 9.37,
@@ -39,8 +51,6 @@ export class RefundManagerService {
                 }
             },
             {
-                month: 'Octobre',
-                year: 2025,
                 refundData: {
                     wording: 'Frais de paiement',
                     amount: 6.00,
@@ -48,8 +58,6 @@ export class RefundManagerService {
                 }
             },
             {
-                month: 'Octobre',
-                year: 2025,
                 refundData: {
                     wording: 'Frais de paiement',
                     amount: 4.00,
@@ -58,5 +66,44 @@ export class RefundManagerService {
             }
         ]
     })
+
+    constructor() {
+    }
+
+    public getRefundList() {
+        let refundManagerList = this.refundManagerList();
+        if (refundManagerList.totalAmount === 0 && refundManagerList.refundManagerData.length !== 0) {
+            return this.refundCalculate(refundManagerList);
+        } else {
+            return refundManagerList
+        }
+    }
+
+    public refundCalculate(refundManagerList: RefundManager) {
+        const initialValue = 0;
+        refundManagerList.totalAmount = refundManagerList.refundManagerData.reduce(
+            (accumulator: number, currentValue: any) => {
+
+                return accumulator + +currentValue.refundData.amount;
+            },
+            initialValue
+        );
+
+        return refundManagerList
+    }
+
+    public currentMonth() {
+        let month = (this.date.getMonth() + 1).toString().padStart(2, "0")
+        return this.monthsList[+month - 1]
+    } 
+
+    public addNewRefund(refund: refundManagerData) {
+        this.refundManagerList().refundManagerData.push(refund);
+        this.refundCalculate(this.refundManagerList())
+    }
+
+    public refundUpdate(newRefund: number) {
+        this.refundManagerList().totalAmount = newRefund;
+    }
 
 }
